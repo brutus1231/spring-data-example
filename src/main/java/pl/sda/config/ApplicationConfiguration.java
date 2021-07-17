3557964e20c18ba2b9a7262afc72aa9cea0b8697
Zdzislaw Trzaska <ztrzaska2@gmail.com>
2021-07-17 10:50:56 +0200

    Initial commit

--- src/main/java/pl/sda/config/ApplicationConfiguration.java
+++ src/main/java/pl/sda/config/ApplicationConfiguration.java
@@ -1,12 +1,43 @@
 package pl.sda.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.instrument.classloading.InstrumentationLoadTimeWeaver;
+import org.springframework.instrument.classloading.LoadTimeWeaver;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 
 @Configuration
 @ComponentScan("pl.sda")
 @EnableJpaRepositories("pl.sda.repository")
 public class ApplicationConfiguration {
 
+    @Bean
+    public JpaTransactionManager transactionManager() {
+        return new JpaTransactionManager(
+                entityManagerFactory().getObject());
+    }
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+        LocalContainerEntityManagerFactoryBean lcemfb = new LocalContainerEntityManagerFactoryBean();
+        lcemfb.setDataSource(dataSource());
+        lcemfb.setPersistenceUnitName("myDatabase");
+        LoadTimeWeaver loadTimeWeaver = new InstrumentationLoadTimeWeaver();
+        lcemfb.setLoadTimeWeaver(loadTimeWeaver);
+        return lcemfb;
+    }
+
+    @Bean
+    public DriverManagerDataSource dataSource() {
+        DriverManagerDataSource ret = new DriverManagerDataSource();
+        ret.setDriverClassName("org.h2.Driver");
+        ret.setUsername("sa");
+        ret.setPassword("");
+        ret.setUrl("jdbc:h2:tcp://localhost/~/test");
+        return ret;
+    }
 }
